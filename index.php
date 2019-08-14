f6a84c4a5cb6df39a510cfc002d941a4e4071041
Rainangramacho <rai_bomfim@hotmail.com>
2019-08-13 23:34:47 -0300

    Changing index file

--- index.php
+++ index.php
@@ -5,17 +5,37 @@
    define('DB_NAME'        , "teste");
    define('DB_DRIVER'      , "sqlsrv");
   
-   require_once "Conexao.php";
+   
+
+   $serverName = "DESKTOP-AC2140U";
+$connectionInfo = array( "Database"=>"teste", "UID"=>"sa", "PWD"=>"rainanbgs");
+$conn = sqlsrv_connect( $serverName, $connectionInfo );
+if( $conn === false ) {
+    die( print_r( sqlsrv_errors(), true));
+}
   
    try{
   
-       $Conexao    = Conexao::getConnection();
-       $query      = $Conexao->query("SELECT * FROM Tabela_teste");
-       $produtos   = $query->fetchAll();
+      
+       $query = "SELECT * FROM Tabela_teste";
+       
+      
+       
+$stmt = sqlsrv_query( $conn, $query );
+if( $stmt === false) {
+    die( print_r( sqlsrv_errors(), true) );
+}
+
+while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC) ) {
+      echo $row['id']."<br />";
+}
+
+sqlsrv_free_stmt( $stmt);
   
    }catch(Exception $e){
        echo $e->getMessage();
        exit;
    }
   
+   
 ?>
